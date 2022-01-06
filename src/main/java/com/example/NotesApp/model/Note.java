5ab1bef462f567284de8c8afa0d16f05df9ee77a
Daniel Matuszewski <39099569+strus100@users.noreply.github.com>
2022-01-06 17:44:44 +0100

    Move to HSQLDB and repair small bug

--- src/main/java/com/example/NotesApp/model/Note.java
+++ src/main/java/com/example/NotesApp/model/Note.java
@@ -2,6 +2,7 @@ package com.example.NotesApp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import javax.persistence.Table;
 @Entity
 @Table
 @AllArgsConstructor
+@NoArgsConstructor
 public class Note
 {
 	@Id
