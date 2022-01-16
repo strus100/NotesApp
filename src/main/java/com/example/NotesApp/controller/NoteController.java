957f9e1faf76865709419dc72ed40a1e06310118
Djradki <djradki@gmail.com>
2022-01-16 13:21:31 +0100

    Quick DB fix. Angular front-end init.

--- src/main/java/com/example/NotesApp/controller/NoteController.java
+++ src/main/java/com/example/NotesApp/controller/NoteController.java
@@ -39,5 +39,4 @@ public class NoteController
 	{
 		noteService.deleteNote( aId );
 	}
-
 }
--- src/main/java/com/example/NotesApp/model/Note.java
+++ src/main/java/com/example/NotesApp/model/Note.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -17,7 +14,7 @@ import javax.persistence.Table;
 public class Note
 {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String title;
 	private String content;
